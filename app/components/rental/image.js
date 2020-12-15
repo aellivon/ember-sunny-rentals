ed3ffde3170898bbb66e7151d8dc422ea9224b1f
Aellivon Kyle Sun <aellivon@gmail.com>
2020-12-16 04:11:08 +0800

    adds interactive components

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,4 +1,12 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
+    // Tracks the variable to reupdate components that rely on this
+    @tracked isLarge = false;
+
+    @action toggleSize() {
+        this.isLarge = !this.isLarge;
+    }
 }
